f9766b26e7b7c5fbfe0af155c5f50ce84714244e
David James <david@createwithplay.com>
2026-01-29 10:24:33 +0000

    Initial Commit

---
+++ Cooked/CookedApp.swift
@@ -0,0 +1,32 @@
+//
+//  CookedApp.swift
+//  Cooked
+//
+//  Created by David James on 29/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CookedApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CookedTests/CookedTests.swift
@@ -0,0 +1,16 @@
+//
+//  CookedTests.swift
+//  CookedTests
+//
+//  Created by David James on 29/01/2026.
+//
+
+import Testing
+
+struct CookedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
